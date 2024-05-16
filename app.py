8f178f75d348c7f85f1d359b08c636d283076ed7
Sergio Lopez <sergioska81@hotmail.com>
2024-05-16 16:47:30 -0600

    feat: Add navbar component for UniBank application

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from components import MySQLClient,SessionHandler
+from elements import navbar
 
 
 st.set_page_config(page_title="UniBank", page_icon=":bank:", layout="wide")
@@ -16,24 +17,46 @@ if 'session' not in st.session_state:
     # Create a new session handler
     st.session_state.session = SessionHandler(client)
 
-if st.session_state.session.is_active():
-    st.write(st.session_state.session.get_session_owner())
 
+navbar(st.session_state.session)
 def heromain():
-    cols = st.columns([.6,.4])
+    cols = st.columns([.4,.6])
     
-    with cols[0]:
-        st.title("UniBank - Tu banco digital")
-        st.caption("Bienvenido a UniBank, tu banco digital. Aquí podrás realizar todas tus operaciones bancarias de manera rápida y segura.")
-        b1, b2 = st.columns([1,1])
+    with cols[1]:
+        style = """<style>
+                .header-hero {
+                    font-size: 10vw;
+                    font-weight: bold;
+                    color: #1E90FF;
+                    text-align: center;
+                    margin: 0;
+                }
+                .caption-hero {
+                    font-size: 16px;
+                    color: #1E90FF;
+                    text-align: center;
+                }
+                </style>
+                """
+        st.markdown(style, unsafe_allow_html=True)
+
+        st.markdown("<p class='header-hero'>UniBank</p>", unsafe_allow_html=True)
+        st.caption("""
+                   <p style='text-align: center;'>
+                   Bienvenido a UniBank, tu banco digital. Aquí podrás realizar todas tus operaciones bancarias de manera rápida y segura.
+                   </p>
+                   """, unsafe_allow_html=True)
+        _, b1,b2 = st.columns([1,1,1])
         with b1:
-            st.button("Iniciar sesión")
+            log = st.button("Iniciar sesión",use_container_width=True)
+            if log:
+                st.switch_page("pages/login.py")
         with b2:
-            reg = st.button("Registrarse")
+            reg = st.button("Registrarse",use_container_width=True)
             if reg:
                 st.switch_page("pages/register.py")
             
-    with cols[1]:
+    with cols[0]:
         st.image("https://img.freepik.com/free-vector/linear-flat-internet-banking-infographics-template-icons-website-hero-image-vector-illustration_126523-2982.jpg",
                  use_column_width=True)
 
---
+++ elements/navbar.py
@@ -0,0 +1,51 @@
+import streamlit as st
+from components import SessionHandler
+
+def navbar(session: SessionHandler):
+    
+    container = st.container(border=True)
+    cols = container.columns([2,1,1,1,1,1,1])
+    
+    
+    with cols[0]:
+        st.markdown("""
+                    <style>
+                    .logo-nav {
+                        font-size: 25px;
+                        font-weight: bold;
+                        color: #1E90FF;
+                        text-align: center;
+                        display: flex;
+                        justify-content: left;
+                        align-items: left;
+                        margin: 0 0 0 0;
+                    }
+                    </style>
+                    """, unsafe_allow_html=True)
+        
+        if session.is_active():
+            st.markdown(f":blue[{session.get_session_owner()}]", unsafe_allow_html=True)
+        else:
+            st.markdown("<p class='logo-nav'>UniBank</p>", unsafe_allow_html=True)
+            
+        if session.is_active():            
+            with cols[2]:
+                    st.page_link("pages/register.py",icon="🏠",label="Inicio")
+            with cols[3]:
+                st.page_link("pages/register.py",icon="💸",label="Transferir")
+            
+            with cols[4]:
+                st.page_link("pages/register.py",icon="📈",label="Historial")
+            
+            with cols[5]:
+                st.page_link("pages/register.py",label="Ver Perfil",icon="👤")
+            
+            with cols[-1]:
+                st.button("Salir🚪",use_container_width=True)
+        else:
+
+            with cols[-1]:
+                st.page_link("pages/login.py",icon="🔒",label="Login")
+            with cols[-2]:
+                st.page_link("pages/register.py",icon="📝",label="Registro")
+                

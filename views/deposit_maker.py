dfafcdfb65bd08a0b4806bf10d605dc912701cac
Sergio Lopez <sergioska81@hotmail.com>
2024-05-18 19:44:12 -0600

    chore: Refactor deposit_maker view function
    Refactor the `deposit_maker` view function in `deposit_maker.py` to improve the deposit functionality in the UniBank application. The changes include adding a button key, handling successful and failed deposits, and rerunning the page after a successful deposit.

--- views/deposit_maker.py
+++ views/deposit_maker.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 from components import MySQLClient
 
@@ -7,9 +8,13 @@ def deposit_maker(client: MySQLClient):
     accs = [f"{account[2]} - {account[0]}" for account in accounts]
     acc = st.selectbox("Selecciona la cuenta a la que deseas depositar", accs)
     amount = st.number_input("Cantidad a depositar",min_value=1.0)
-    deposit = st.button("Depositar")
+    deposit = st.button("Depositar",use_container_width=True,key="depositpersonal")
     if deposit:
         acc_id = acc.split("-")[1].strip()
         data = client.make_depositer(cuenta=acc_id,monto=amount)
-        st.write(data)
-        st.success("Deposito realizado exitosamente")
+        if data:
+            st.success("Deposito realizado exitosamente")
+            time.sleep(3)
+            st.rerun()
+        else:
+            st.error("Fallo al realizar deposito, sus fondos no fueron afectados")
